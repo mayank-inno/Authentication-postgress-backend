3c6861badc0f58184b007959fef368513dc096c7
Mayank Behl <mayank.behl@innostax.com>
2025-11-21 19:03:03 +0530

    .

--- Controllers/User.js
+++ Controllers/User.js
@@ -232,7 +232,7 @@ export const login = async (req, res) => {
                         httpOnly: true,
                         secure: process.env.Environment == "production",
                         maxAge: 7 * 24 * 60 * 60 * 1000,
-                        sameSite: "none",
+                        sameSite: "lax",
                     })
                     return res.status(200).json({
                         success: true,
