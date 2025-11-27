083286fe88040b088d3ecb8530d78dd2096e3217
Mayank Behl <mayank.behl@innostax.com>
2025-11-27 17:34:28 +0530

    added same site none hardcode

--- Controllers/User.js
+++ Controllers/User.js
@@ -233,7 +233,7 @@ export const login = async (req, res) => {
                         httpOnly: true,
                         secure: process.env.Environment == "production",
                         maxAge: 7 * 24 * 60 * 60 * 1000,
-                        sameSite: "lax",
+                        sameSite: "none",
                     })
                     return res.status(200).json({
                         success: true,
